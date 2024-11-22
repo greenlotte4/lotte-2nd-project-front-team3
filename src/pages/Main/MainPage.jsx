925450e550a56c999cea3473b7f1692012c98812
subin3578 <ghkdtnqls95@gmail.com>
2024-11-22 18:55:24 +0900

    header menu

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,7 +1,5 @@
-import AntWorkLayout from "../../layouts/AntWorkSubLayout";
-
+import AntWorkLayout from "../../layouts/AntWorkLayout";
 import MainSection from "./../../components/main/mainSection";
-
 export default function MainPage() {
   return (
     <>
--- src/pages/Main/Paging/pagingPage.jsx
+++ src/pages/Main/Paging/pagingPage.jsx
@@ -1,12 +1,10 @@
 import AntWorkLayout from "../../../layouts/AntWorkLayout";
-import PageAside from "./../../../components/common/pageAside";
 import PagingSection from "./../../../components/paging/pagingSection";
 
 export default function PagingPage() {
   return (
     <>
       <AntWorkLayout>
-        <PageAside />
         <PagingSection />
       </AntWorkLayout>
     </>
