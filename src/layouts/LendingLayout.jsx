2c5282fbd3d8526c0546ed95e71ef60e73de9f0c
junhyeokkk <loveu9911@naver.com>
2024-11-27 11:38:01 +0900

    요금페이지 완성

--- src/layouts/LendingLayout.jsx
+++ src/layouts/LendingLayout.jsx
@@ -1,6 +1,13 @@
 import LendingFooter from "../components/landing/Lendingfooter";
 import LendingHeader from "../components/landing/Lendingheader";
+{
+  /*
+    날짜 : 2024/11/27(수)
+    생성자 : 최준혁
+    내용 : LendingLayout 추가
 
+  */
+}
 const LendingLayout = ({ children }) => {
   return (
     <div id="wrap">
