bda97c2ef8f5c282a961d2403dc97c2cf8673236
subin3578 <ghkdtnqls95@gmail.com>
2024-11-27 11:07:39 +0900

    resolve conflicts

--- src/pages/Main/setting/settingMainPage.jsx
+++ src/pages/Main/setting/settingMainPage.jsx
@@ -1,4 +1,3 @@
-import AdminMain from "../../../components/main/admin/AdminMain";
 import SettingMain from "../../../components/main/setting/SettingMain";
 import AntWorkLayout from "../../../layouts/AntWorkLayout";
 
