6dd18546615b3fc708a827778e3bd3862ffd4364
ekkang2 <rkddmsrud27@gmail.com>
2024-11-28 16:18:26 +0900

    배포 test

--- src/components/main/project/projectMainSection.jsx
+++ src/components/main/project/projectMainSection.jsx
@@ -8,7 +8,7 @@ export default function ProjectMainSection() {
           <div className="max-w-9xl mx-auto p-6">
             <div className="mb-6">
               <h1 className="text-xl font-semibold">My Projects</h1>
-              <p className="text-sm text-gray-500"> 내가 참여중인 프로젝트!</p>
+              <p className="text-sm text-gray-500"> 내가 참여중인 프로젝트</p>
             </div>
 
             <div className="relative mb-6 flex items-center">
